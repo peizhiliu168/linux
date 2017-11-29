ca7b9625dc9c538dcbed1730677357975bd0dd0a
Jens Wiklander <jens.wiklander@linaro.org>
2017-11-29 14:48:25 +0200

    tee: flexible shared memory pool creation
    Makes creation of shm pools more flexible by adding new more primitive
    functions to allocate a shm pool. This makes it easier to add driver
    specific shm pool management.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Signed-off-by: Volodymyr Babchuk <vlad.babchuk@gmail.com>
    (cherry picked from commit 587e439e70a43ac78f9613074895fdd61f75f981)

--- drivers/tee/tee_shm.c
+++ drivers/tee/tee_shm.c
@@ -14,41 +14,20 @@
 static void tee_shm_release(struct tee_shm *shm)
 {
 	struct tee_device *teedev = shm->teedev;
-
+	struct tee_shm_pool_mgr *poolm;
 	mutex_lock(&teedev->mutex);
 	idr_remove(&teedev->idr, shm->id);
 	if (shm->ctx)
 		list_del(&shm->link);
 	mutex_unlock(&teedev->mutex);
 
-	if (shm->flags & TEE_SHM_POOL) {
-		struct tee_shm_pool_mgr *poolm;
+	if (shm->flags & TEE_SHM_DMA_BUF)
+		poolm = teedev->pool->dma_buf_mgr;
+	else
+		poolm = teedev->pool->private_mgr;
 
-		if (shm->flags & TEE_SHM_DMA_BUF)
-			poolm = teedev->pool->dma_buf_mgr;
-		else
-			poolm = teedev->pool->private_mgr;
-
-		poolm->ops->free(poolm, shm);
-	} else if (shm->flags & TEE_SHM_REGISTER) {
-		size_t n;
-		int rc = teedev->desc->ops->shm_unregister(shm->ctx, shm);
-
-		if (rc)
-			dev_err(teedev->dev.parent,
-				"unregister shm %p failed: %d", shm, rc);
-
-		for (n = 0; n < shm->num_pages; n++)
-			put_page(shm->pages[n]);
-
-		kfree(shm->pages);
-	}
-
-	if (shm->ctx)
-		teedev_ctx_put(shm->ctx);
-
+	poolm->ops->free(poolm, shm);
 	kfree(shm);
-
 	tee_device_put(teedev);
 }
 
